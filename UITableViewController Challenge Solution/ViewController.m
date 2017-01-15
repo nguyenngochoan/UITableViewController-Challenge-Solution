f1076751f88a9686892ae04d5e18bdbb25a3016e
nguyenngochoan <n.nhoan.p8@gmail.com>
2017-01-15 17:25:41 +0100

    create project

---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by nguyen ngoc hoan on 15/01/2017.
+//  Copyright © 2017 nguyen ngoc hoan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by nguyen ngoc hoan on 15/01/2017.
+//  Copyright © 2017 nguyen ngoc hoan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
